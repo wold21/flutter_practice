49a44ee0c592a7e0adc835998842efc13d07852c
wold21 <wold2180@gmail.com>
2021-10-23 15:40:08 +0900

    photo content

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600]),
-        ),
+        child: Image.asset('assets/moon.jpg'),
       ),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         child: Text('click'),
       ),
-    )));
+    );
+  }
+}
